685397f005c90312129a690ba89c21fbcc4b3281
Sigitas Grincevičius <s.grincevicius@gmail.com>
2023-04-04 21:52:26 +0300

    Added entities array to js file

---
+++ js/script.js
@@ -0,0 +1,25 @@
+let entities = [{
+   imagePath: "./images/image-rostov-admiral-project.png",
+   description: "Only a small part of the work performed by our company is presented<br>on the site. For 14 years on in the construction<br>market we have made happy more than 1000 families",
+   city: "Rostov-on-Don<br>LCD admiral",
+   area: "81 m2",
+   repairTime: "3.5 months",
+   repairCost: "Upon request"
+},
+{
+   imagePath: "./images/image-sochi-project.png",
+   description: "Only a small part of the work performed by our company is presented<br>on the site. For 14 years on in the construction<br>market we have made happy more than 1000 families",
+   city: "Sochi<br>Thieves",
+   area: "105 m2",
+   repairTime: "4 months",
+   repairCost: "Upon request"
+},
+{
+   imagePath: "./images/image-rostov-patriotic-project.png",
+   description: "Only a small part of the work performed by our company is presented<br>on the site. For 14 years on in the construction<br>market we have made happy more than 1000 families",
+   city: "Rostov-on-Don<br>Patriotic",
+   area: "93 m2",
+   repairTime: "3 months",
+   repairCost: "Upon request"
+}
+]
